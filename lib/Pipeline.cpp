0dc6ae59861daa061a6534b677b8034c12b8a9cb
Tony <tonyqin@ibm.com>
2026-08-11 15:22:42 -0400

    added command line prompt
    Signed-off-by: Tony <tonyqin@ibm.com>

--- lib/Pipeline.cpp
+++ lib/Pipeline.cpp
@@ -22,6 +22,12 @@
 
 #include "dataflow-scheduler/Pipeline.h"
 
+#include <filesystem>
+
+#include <llvm/Support/CommandLine.h>
+#include <llvm/Support/raw_ostream.h>
+#include <mlir/IR/BuiltinOps.h>
+#include <mlir/Pass/Pass.h>
 #include <mlir/Pass/PassManager.h>
 #include <mlir/Transforms/Passes.h>
 
@@ -33,7 +39,48 @@
 
 using namespace scheduler;
 
-void scheduler::buildKTDPToScheduleIRPipeline(
+static llvm::cl::opt<std::string> DumpScheduleIRDir(
+    "dump-schedule-ir-dir",
+    llvm::cl::desc("Directory to write schedule_ir.mlir into after address "
+                   "assignment and before lowering to DFIR"),
+    llvm::cl::init(""));
+
+namespace {
+struct DumpScheduleIRPass
+    : public mlir::PassWrapper<DumpScheduleIRPass,
+                               mlir::OperationPass<mlir::ModuleOp>> {
+  MLIR_DEFINE_EXPLICIT_INTERNAL_INLINE_TYPE_ID(DumpScheduleIRPass)
+
+  explicit DumpScheduleIRPass(llvm::StringRef dir) : dir_(dir.str()) {}
+
+  auto getArgument() const -> llvm::StringRef final {
+    return "dump-schedule-ir";
+  }
+  auto getDescription() const -> llvm::StringRef final {
+    return "Write the ScheduleIR module to a file";
+  }
+
+  void runOnOperation() override {
+    std::error_code ec;
+    std::filesystem::create_directories(dir_, ec);
+    const auto path =
+        (std::filesystem::path(dir_) / "schedule_ir.mlir").string();
+    llvm::raw_fd_ostream out(path, ec);
+    if (ec) {
+      llvm::errs() << "dump-schedule-ir: could not write " << path << ": "
+                   << ec.message() << "\n";
+      return;
+    }
+    getOperation()->print(out);
+    out << "\n";
+  }
+
+ private:
+  std::string dir_;
+};
+}  // namespace
+
+void scheduler::buildKTDPToDFIRPipeline(
     mlir::OpPassManager& pm, const SchedulerExtContext& scheduler_ctx) {
   pm.addPass(createKTIRLegalityCheckPass());
   pm.addPass(createComputeGroupExtractionPass());
@@ -49,8 +96,6 @@ void scheduler::buildKTDPToScheduleIRPipeline(
   pm.addPass(mlir::createCanonicalizerPass());
   pm.addPass(mlir::createLoopInvariantCodeMotionPass());
   pm.addPass(mlir::ktdf::createStageCoarseningPass());
-  pm.addPass(mlir::ktdf::createReductionLoopExposurePass());
-  pm.addPass(mlir::ktdf::createMapReductionPartialsPass());
   pm.addPass(mlir::ktdf::createBroadcastPromotionPass());
   pm.addPass(createDoubleBufferingPass(scheduler_ctx));
   // Parallelizing before tile selection is beneficial because the tile size
@@ -62,11 +107,8 @@ void scheduler::buildKTDPToScheduleIRPipeline(
   pm.addPass(createAffineMinCanonicalizationPass());
   pm.addPass(mlir::ktdf::createSubsumeLinearizeIndexPass());
   pm.addPass(createAddressAssignmentPass(scheduler_ctx));
-}
-
-void scheduler::buildKTDPToDFIRPipeline(
-    mlir::OpPassManager& pm, const SchedulerExtContext& scheduler_ctx) {
-  buildKTDPToScheduleIRPipeline(pm, scheduler_ctx);
+  if (!DumpScheduleIRDir.empty())
+    pm.addPass(std::make_unique<DumpScheduleIRPass>(DumpScheduleIRDir));
   // TODO: position of cross-instance parallelization is TBD
   // pm.addPass(createParallelizeLoopsAcrossInstancesPass(scheduler_ctx));
   pm.addPass(createNormalizeGridTo1DPass());
